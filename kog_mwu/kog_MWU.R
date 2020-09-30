eaed5a0de7135616c0b2c2de388c132b0b0ee475
Groves Dixon <grovesdixon@grovess-mbp.attlocal.net>
2020-09-30 07:35:58 -0400

    revise kog_MWU.R for clarity

--- kog_mwu/kog_MWU.R
+++ kog_mwu/kog_MWU.R
@@ -4,16 +4,18 @@ source('figurePlotting/rna_functions.R')
 library(KOGMWU)
 
 
-#run single file 
-inputFile = 'wgcna/go_mwu_module_style/MMred_goInput.csv'
-inputFile = 'go_mwu/clusterAstress_For_MWU.csv'
+#first run single file 
+inputFile = 'wgcna/go_mwu_module_style/MMred_goInput.csv' #the red module
+inputFile = 'go_mwu/clusterAstress_For_MWU.csv'           #all type A stress samples together
 kogFile = 'kog_mwu/Amil.v2.eggnogWebsite.gene2kog.tsv' 
 inputData = read.csv(inputFile)
 gene2kog = read.table(kogFile, sep='\t', header = TRUE)
 
 M = kog.mwu(data=read.csv(inputFile), gene2kog=gene2kog, Alternative='t')
 M
-################# Generate heatmap
+
+
+## Generate heatmap
 # compiling a table of delta-ranks to compare these results:
 kognames=M$term
 kogtable=data.frame("M" = M$delta.rank)
@@ -47,24 +49,21 @@ kogsToRemove = c('Function Unknown',
 
 
 # RUN FOR MULTIPLE FILES --------------------------------------------------
-ll=load('./go_mwu/groupedInputs.Rdata')
-ll
-ll=load('./go_mwu/individualInputs.Rdata')
-ll
-groupedNames
-individualNames
-names = groupedNames
-names = individualNames
-names = append(individualNames, c('clusterAstress', 'clusterBstress'))
-inputs = paste(paste('./go_mwu', names,sep='/'), '_For_MWU.csv', sep='')
+#build a heatmap for individual studies or analyses based on groups os studies
 
-# #optionally add the modules
-# modNames = c('red_module', 'white_module', 'green_module')
-# modPaths = c('wgcna/go_mwu_module_style/MMred_goInput.csv',
-#              'wgcna/go_mwu_module_style/MMwhite_goInput.csv',
-#              'wgcna/go_mwu_module_style/MMgreen4_goInput.csv')
-# names = append(names, modNames)
-# inputs = append(inputs, modPaths)
+#grouped (these are less interesting, but kept for reference)
+# ll=load('./go_mwu/groupedInputs.Rdata')
+# groupedNames
+# names = groupedNames
+
+#load KOG input for each individual study and for the two clusters run in full
+ll=load('./go_mwu/individualInputs.Rdata') 
+ll
+individualNames
+names = individualNames
+names = append(individualNames, c('clusterAstress', 'clusterBstress')) #add type A and type B stress 
+inputs = paste(paste('./go_mwu', names,sep='/'), '_For_MWU.csv', sep='')
+inputs #these are the KOG input files being used
 
 
 kogResults = list()
@@ -106,6 +105,7 @@ obyA = kmat %>%
 orderedNames=obyA
 
 
+#build the heatmap
 kmat=kmat[orderedNames,]
 plot(density(kmat))
 BREAKS=seq(-1500, 1500, length.out=length(color)+1)
@@ -121,10 +121,7 @@ pheatmap(kmat.ordered,
          breaks = BREAKS
          )
 
-
-
-
-#plot to match other heatmap
+#re-plot to match main heatmap
 ll=load('kog_mwu/heatmapObjects.Rdata')
 ll
 c=kmat[orderedNames,orderedProjects]
@@ -154,9 +151,7 @@ pheatmap(c,
          treeheight_row=25,
          treeheight_col=0)
 
-
-
-#subset
+#subset for only the type A studies
 ll=load('metadata/corStressProjs.Rdata')
 ll
 corStressProjs
@@ -178,7 +173,7 @@ pheatmap(ksub.ordered,
          breaks=BREAKS
          # labels_row=kdat$term
 )
-
+#the differences here are potentially of interest
 
 
 # RUN FOR YEAST DATA ------------------------------------------------------
@@ -272,7 +267,8 @@ adat %>%
   filter(`COG cat`=='Y') %>% 
   write_csv(path='~/Desktop/nucStruc.csv')
 
-# plot a selection --------------------------------------------------------
+# plot a comparisons --------------------------------------------------------
+#here we want to compare the stress types with yeast
 
 #order by A
 obyA = kmat %>% 
@@ -294,6 +290,8 @@ selection = c('All_yeast',
               'clusterBstress')
 smat=mat[orderedNames,selection]
 
+
+#plot heatmap
 BREAKS=seq(-1000, 1000, length.out=length(color)+1)
 smat=smat[,c("clusterAstress", "clusterBstress", "All_yeast")]
 COLLABS = c('type A', 'type B', 'yeast')
@@ -310,10 +308,9 @@ pheatmap(smat,
          
 )
 
+#plot scatterplots
 plotdf = data.frame(smat)
 colnames(plotdf) = c('typeA', 'typeB', 'yeast')
-
-
 ab = plot_scatter(plotdf, xcol='typeA', ycol='typeB',  addStars=TRUE) + labs(x='type A', y='type B')
 lmab = lm(typeB~typeA, data=plotdf)
 summary(lmab)
@@ -326,7 +323,6 @@ summary(lmby)
 pltList = list(ab, ay, by)
 BREAKS=c(-1500, 0, 1500)
 LIMS=c(-2100, 2100)
-
 pltList2 = lapply(pltList, function(x) return(x +
                                                 scale_x_continuous(breaks=BREAKS, limits=LIMS) +
                                                 scale_y_continuous(breaks = BREAKS, limits=LIMS) +
@@ -334,8 +330,7 @@ pltList2 = lapply(pltList, function(x) return(x +
 plot_grid(plotlist=pltList2, nrow=3)
 
 
-
-
+#scatterplot matrix
 dat = smat
 pairs(smat)
 dat %>% 
@@ -346,144 +341,3 @@ dat %>%
 pairs(smat)
 
 
-# RUN FOR INDIVIDUAL PROJECTS ---------------------------------------------
-
-inputs = list.files('go_mwu', '*_IndStudy_ForMWU.csv', full.names = TRUE)
-names = sub('_IndStudy_ForMWU.csv', '', list.files('go_mwu', '*_IndStudy_ForMWU.csv'))
-kogFile = 'kog_mwu/Amillepora.v2.00.chrs_gene2kogClass.tab' 
-kogResults = list()
-for (i in 1:length(inputs)){
-  inputFile=inputs[i]
-  name = names[i]
-  cname= paste(name, 'DR', sep='_')
-  M=kog.mwu(inname = inputFile, gene2kog = kogFile, Alternative='two.sided')
-  M[,cname]=M$delta.rank
-  M$term=as.character(M$term)
-  kogResults[[cname]]=M
-}
-
-kdat = kogResults %>% 
-  purrr::reduce(full_join, by='term') %>% 
-  select('term', ends_with("_DR"))
-
-
-kdat %>% 
-  select(grep('_DR', colnames(kdat)))
-
-colnames(kdat)=sub('_DR', '', colnames(kdat))
-colnames(kdat)
-
-kmat = kdat %>% 
-  select(-'term') %>% 
-  as.matrix()
-rownames(kmat)=kdat$term
-meds = apply(kmat, 1, median)
-orderedNames = names(meds)[order(meds)]
-kmat=kmat[orderedNames,]
-
-#build heatmap
-library(pheatmap)
-library(RColorBrewer)
-color = colorRampPalette(rev(c(brewer.pal(n = 7, name = "RdYlBu"),"darkblue","darkblue")))(100)
-pheatmap(kmat, clustering_distance_rows = "euclidean",
-         cluster_rows = FALSE,
-         cluster_cols = TRUE,
-         color=color,
-         treeheight_row=15,
-         treeheight_col=20,
-         # labels_row=kdat$term
-)
-
-
-#subset
-keep=c('adultVlarva',
-       'stress',
-       'heat',
-       'cold',
-       'salinity',
-       'immune',
-       'ph'
-)
-ksub=kmat[,keep]
-pheatmap(ksub, clustering_distance_rows = "euclidean",
-         cluster_rows = FALSE,
-         cluster_cols = TRUE,
-         color=color,
-         treeheight_row=15,
-         treeheight_col=20,
-         # labels_row=kdat$term
-)
-
-
-# plot together yeast and individual -----------------------------------------------------------
-
-head(y.kmat)
-head(kmat)
-
-mat = merge(kmat, y.kmat, by = 0)
-rownames(mat)=mat$Row.names
-mat$Row.names<-NULL
-dim(mat)
-meds = apply(mat, 1, median)
-orderedNames = names(meds)[order(meds)]
-mat=mat[orderedNames,]
-pheatmap(mat, clustering_distance_rows = "euclidean",
-         cluster_rows = FALSE,
-         cluster_cols = TRUE,
-         color=color,
-         treeheight_row=15,
-         treeheight_col=20,
-         # labels_row=kdat$term
-)
-
-
-
-selectCoral = colnames(kmat)
-
-
-smat = mat[,selectCoral]
-ymat = mat[,colnames(y.kmat)]
-ymeds = apply(ymat, 1, median)
-
-
-
-smat$yeast = ymeds
-
-dat = smat
-dat %>% 
-  gather(key='stress', value='deltaRank', selectCoral) %>% 
-  filter(stress=='heat_NoBEWW') %>% 
-  ggplot(aes(x=yeast, y=deltaRank, group=stress, color=stress)) +
-  geom_point()
-
-
-
-head(kmat)
-
-
-lm1=lm(mat$H_matz_heatTolLat_PRJNA279192~mat$All_yeast)
-summary(lm1)
-p1=mat %>% 
-  ggplot(aes(x=H_matz_heatTolLat_PRJNA279192, y=All_yeast)) +
-  geom_point() +
-  geom_smooth(method='lm')
-
-
-lm1=lm(mat$j1_BLEACH_EWW~mat$All_yeast)
-summary(lm1)
-p2=mat %>% 
-  ggplot(aes(x=j1_BLEACH_EWW, y=All_yeast)) +
-  geom_point()+
-  geom_smooth(method='lm')
-
-
-
-lm1=lm(mat$j1_BLEACH_EWW~mat$H_matz_heatTolLat_PRJNA279192)
-summary(lm1)
-p3=mat %>% 
-  ggplot(aes(x=j1_BLEACH_EWW, y=H_matz_heatTolLat_PRJNA279192)) +
-  geom_point()+
-  geom_smooth(method='lm')
-
-
-plot_grid(p1,p2,p3, nrow=1)
